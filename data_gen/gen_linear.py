6ccd1d6f45dcfb434ad6da8b0576968a23c2659f
Avikalp Gupta <avikalp.gupta@freshgravity.com>
2017-12-26 21:54:21 +0530

    Created the gen_linear.py file to generate data. Also created a CSV file using the same

---
+++ data_gen/gen_linear.py
@@ -0,0 +1,50 @@
+import numpy as np
+from random import randint
+import matplotlib.pyplot as plt
+import csv
+
+def gen(m, c, domain, density, var):
+	data = []
+	for x in xrange(0, domain):
+		rep = randint(0,density)
+		for j in xrange(0, rep):
+			scale = randint(0, var)
+			y = (m * x) + c + (scale * np.random.normal())
+			data.append((x, y))
+        return data
+
+def show(data):
+	X = []
+	Y = []
+	for dep, indep in data:
+		X.append(dep)
+		Y.append(indep)
+	plt.plot(X, Y, 'ro')
+	plt.show()
+
+def genCSV(data, filename = ''):
+    if filename == '':
+        filename = "data_" + len(data) + ".csv"
+    else:
+        filename = filename+".csv"
+    with open(filename, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        for dep, indep in data:
+            writer.writerow([dep, indep])
+
+def main():
+	m = 1.0
+	c = 2.0
+        domain = 1000
+        density = 2
+        variance_measure = 250
+	data = gen(m, c, domain, density, variance_measure)
+        print len(data)
+	show(data)
+        
+        # Naming convention: "data_<Num of Points>_<m>_<c>_<x-range>_<density>_<variance>"
+        filename = "data_"+str(len(data))+"_"+str(m)+"_"+str(c)+"_"+str(domain)+"_"+str(density)+"_"+str(variance_measure)
+        genCSV(data, filename)
+
+
+main()
